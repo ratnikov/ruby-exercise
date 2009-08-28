6dc9216a5b9e1a2ee9aac26dc9eb8a8239e21931
ratnikov <ratnikov@gmail.com>
2009-08-28 17:17:27 -0500

    Added tests about other attribute assignments. Refactored a bit

--- src/person.rb
+++ src/person.rb
@@ -1,14 +1,26 @@
 require 'date'
 
 class Person
-  attr_accessor :last, :first, :middle, :gender, :color
-  attr_reader :date_of_birth
+  attr_accessor :last, :first, :middle, :color
+  attr_reader :gender, :date_of_birth
 
   def initialize options = nil
     if options
-      self.gender = parse_gender options[:gender] if options.has_key?(:gender)
-      self.date_of_birth = options[:birth] if options.has_key?(:birth)
+      options.each do |(key, value)|
+	self.send "#{key}=", value
+      end
     end
+  end
+
+  def gender= gender
+    @gender = 
+      case gender
+      when 'Male', 'M' then :male
+      when 'Female', 'F' then :female
+      else
+	raise ArgumentError, "What gender is this: #{gender.inspect}?"
+      end
+    # @gender =
   end
 
   def male?
@@ -18,16 +30,5 @@ class Person
   def date_of_birth= date
     # replace the - with / so that usual date parser can understand it
     @date_of_birth = Date.parse date.gsub(/-/, '/')
-  end
-
-  private
-
-  def parse_gender gender
-    case gender
-    when 'Male', 'M' then :male
-    when 'Female', 'F' then :female
-    else
-      raise ArgumentError, "What gender is this: #{gender.inspect}?"
-    end
   end
 end
--- test/person_test.rb
+++ test/person_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PersonTest < Test::Unit::TestCase
   def test_truth; assert true end
 
-  def test_gender
+  def test_gender_assignment
     assert_equal true, Person.new(:gender => 'M').male?, "Should be male if gender is 'M'"
     assert_equal false, Person.new(:gender => 'F').male?, "Should be female if gender is 'F'"
 
@@ -11,8 +11,14 @@ class PersonTest < Test::Unit::TestCase
     assert_equal false, Person.new(:gender => 'Female').male?, "Should be female if gender is 'Female'"
   end
 
-  def test_date_of_birth
-    assert_equal Date.parse("August 13, 1975"), Person.new(:birth => '8-13-1975').date_of_birth, "Should regognize the dashed format"
-    assert_equal Date.parse("August 13, 1975"), Person.new(:birth => '8/13/1975').date_of_birth, "Should recognize the slash format"
+  def test_date_of_birth_assignment
+    assert_equal Date.parse("August 13, 1975"), Person.new(:date_of_birth => '8-13-1975').date_of_birth, "Should regognize the dashed format"
+    assert_equal Date.parse("August 13, 1975"), Person.new(:date_of_birth => '8/13/1975').date_of_birth, "Should recognize the slash format"
+  end
+
+  def test_usual_attributes
+    assert_equal 'Johnson', Person.new(:last => 'Johnson').last, "Should return last name correctly"
+    assert_equal 'Jack', Person.new(:first => 'Jack').first, "Should return first name correctly"
+    assert_equal 'Red', Person.new(:color => 'Red').color, 'Should return the color correctly'
   end
 end
