303a8fd49eb39c1a4bac434eedde15265ca8c9ca
ratnikov <ratnikov@gmail.com>
2009-08-28 17:10:39 -0500

    Added support for date of birth

--- src/person.rb
+++ src/person.rb
@@ -1,9 +1,13 @@
+require 'date'
+
 class Person
-  attr_accessor :last, :first, :middle, :gender, :color, :date_of_birth
+  attr_accessor :last, :first, :middle, :gender, :color
+  attr_reader :date_of_birth
 
   def initialize options = nil
     if options
       self.gender = parse_gender options[:gender] if options.has_key?(:gender)
+      self.date_of_birth = options[:birth] if options.has_key?(:birth)
     end
   end
 
@@ -11,6 +15,10 @@ class Person
     self.gender == :male
   end
 
+  def date_of_birth= date
+    # replace the - with / so that usual date parser can understand it
+    @date_of_birth = Date.parse date.gsub(/-/, '/')
+  end
 
   private
 
--- test/person_test.rb
+++ test/person_test.rb
@@ -10,4 +10,9 @@ class PersonTest < Test::Unit::TestCase
     assert_equal true, Person.new(:gender => 'Male').male?, "Should be male if gender is 'Male'"
     assert_equal false, Person.new(:gender => 'Female').male?, "Should be female if gender is 'Female'"
   end
+
+  def test_date_of_birth
+    assert_equal Date.parse("August 13, 1975"), Person.new(:birth => '8-13-1975').date_of_birth, "Should regognize the dashed format"
+    assert_equal Date.parse("August 13, 1975"), Person.new(:birth => '8/13/1975').date_of_birth, "Should recognize the slash format"
+  end
 end
